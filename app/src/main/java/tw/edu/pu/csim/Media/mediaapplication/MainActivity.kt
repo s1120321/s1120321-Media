68a83e09fc3089a4271a23d3f3e8cdbdb5a5ac08
s112321 <yieewenhong@gmail.com>
2024-10-28 16:56:18 +0800

    第2次commit

--- app/src/main/java/tw/edu/pu/csim/Media/mediaapplication/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/Media/mediaapplication/MainActivity.kt
@@ -1,17 +1,28 @@
 package tw.edu.pu.csim.Media.mediaapplication
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.w3c.dom.Text
 import tw.edu.pu.csim.Media.mediaapplication.ui.theme.MediaApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,18 +44,100 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-        Text(text = "hi")
+    var Animals = arrayListOf(
+        R.drawable.animal0, R.drawable.animal1,
+        R.drawable.animal2, R.drawable.animal3,
+        R.drawable.animal4, R.drawable.animal5,
+        R.drawable.animal6, R.drawable.animal7,
+        R.drawable.animal8, R.drawable.animal9
+    )
+    var AnimalsName = arrayListOf(
+        "鴨子", "企鵝", "青蛙", "貓頭鷹", "海豚", "牛",
+        "無尾熊", "獅子", "狐狸", "小雞"
+    )
+    val context = LocalContext.current
+    var mper = MediaPlayer()
 
-        for (i in 0..50) {
-            Text(i.toString())
+    Column {
+        Row {
+            Button(onClick = {
+                mper.reset()
+                mper = MediaPlayer.create(context, R.raw.tcyang)
+                mper.start()
+            }) {
+                Text(text = "歡迎修課")
+                Image(
+                    painter = painterResource(id = R.drawable.teacher),
+                    contentDescription = "修課",
+                    Modifier.size(70.dp)
+                )
+            }
+
+            Button(onClick = {
+                mper.reset()
+                mper = MediaPlayer.create(context, R.raw.fly)
+                mper.start()
+            }) {
+                Text(text = "展翅飛翔")
+                Image(
+                    painter = painterResource(id = R.drawable.fly),
+                    contentDescription = "飛翔",
+                    Modifier.size(70.dp)
+                )
+            }
+        }
+
+
+        LazyRow {
+            items(30) { index ->
+                Text(
+                    index.toString(),
+                    modifier = modifier
+                )
+                Text(
+                    text = AnimalsName[index % 10],
+                    modifier = modifier
+                )
+                Image(
+                    painter = painterResource(id = Animals[index % 10]),
+                    contentDescription = "動物0",
+                    modifier = Modifier.fillParentMaxWidth(0.5f)
+                )
+            }
+        }
+
+        LazyColumn {
+            /*
+            item {
+                Image(
+                painter = painterResource(id = R.drawable.animal0),
+                contentDescription = "黃色小鴨"
+                 )
+            }
+
+         */
+            item {
+                Text(
+                    text = "Hello $name!",
+                    modifier = modifier
+                )
+            }
+            item {
+                Text(text = "hi")
+
+            }
+            items(50) { index ->
+                Text(index.toString())
+            }
+            /*
+        item {
+            for (i in 0..50) {
+             Text(i.toString())
+            }
+        }
+        */
         }
     }
-
 }
 @Preview(showBackground = true)
 @Composable
