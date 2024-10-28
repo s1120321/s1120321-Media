a47bd565d9b1513e6980ad30b36cb4effd53caf4
s112321 <yieewenhong@gmail.com>
2024-10-28 14:52:30 +0800

    第1次commit

---
+++ app/src/main/java/tw/edu/pu/csim/Media/mediaapplication/MainActivity.kt
@@ -0,0 +1,55 @@
+package tw.edu.pu.csim.Media.mediaapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tw.edu.pu.csim.Media.mediaapplication.ui.theme.MediaApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MediaApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Text(text = "hi")
+
+        for (i in 0..50) {
+            Text(i.toString())
+        }
+    }
+
+}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MediaApplicationTheme {
+        Greeting("Android")
+    }
+}
